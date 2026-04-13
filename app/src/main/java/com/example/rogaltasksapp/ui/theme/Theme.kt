b2b5399bd293ffa32324f3b5e62beca847ad6504
kamilek7 <kamijar20031@gmail.com>
2026-04-13 15:21:49 +0200

    poprawka do ui

--- app/src/main/java/com/example/rogaltasksapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/rogaltasksapp/ui/theme/Theme.kt
@@ -5,8 +5,6 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
@@ -14,8 +12,11 @@ import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xffffffff),
+    onPrimary = Color.White,
     secondary =  Color(0xff000000),
-    tertiary =  Color(0xffff0000)
+    onSecondary = Color.White,
+    tertiary =  Color(0xffff0000),
+    onTertiary = Color.White,
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -38,15 +39,9 @@ private val LightColorScheme = lightColorScheme(
 fun RogalTasksAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
